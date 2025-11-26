19cff861f04b7b2e0e96c1d5243eb0ddcbe76737
Garima Singal <garimasingal13@gmail.com>
2025-11-26 14:48:55 +0530

    Changes

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+let speech = new SpeechSynthesisUtterance();
+
+let voices = [];
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+  voices = window.speechSynthesis.getVoices();
+
+  voiceSelect.innerHTML = ""; 
+
+  voices.forEach((voice, i) => {
+    let option = new Option(
+      `${voice.name} (${voice.lang})`,
+      i                                
+    );
+    voiceSelect.appendChild(option);
+  });
+
+  // default voice
+  speech.voice = voices[0];
+};
+
+document.querySelector("button").addEventListener("click", () => {
+  speech.text = document.querySelector("textarea").value;
+
+  let selectedIndex = voiceSelect.value;
+  speech.voice = voices[selectedIndex]; // ✅ selected voice
+
+  window.speechSynthesis.speak(speech);
+});
